3172c08b48a5a92154101e20d194b2bb26d34dc1
k-matsumoto-214 <keisuke.matsumoto214+github@gmail.com>
2022-10-12 12:25:28 +0900

    アノテーション修正

--- src/main/java/com/kirin/reservation/ReservationApplication.java
+++ src/main/java/com/kirin/reservation/ReservationApplication.java
@@ -2,11 +2,11 @@ package com.kirin.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@Retryable
+@EnableRetry
 @EnableScheduling
 public class ReservationApplication {
 
