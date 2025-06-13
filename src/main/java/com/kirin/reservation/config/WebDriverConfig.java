fd976eeee8a2cd775be69f8b765a637aaa939bcc
k.matsumoto <91876695+k-matsumoto-214@users.noreply.github.com>
2025-06-13 09:08:19 +0900

    Feature/update (#16)
    * 設定微調整
    
    * ver up
    
    * URL変更に対応
    
    * pom update
    
    * pom update
    
    * pom update
    
    * pom update

--- src/main/java/com/kirin/reservation/config/WebDriverConfig.java
+++ src/main/java/com/kirin/reservation/config/WebDriverConfig.java
@@ -1,38 +1,41 @@
 package com.kirin.reservation.config;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.Duration;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
 @Configuration
 public class WebDriverConfig {
 
-  @Value("${selenium.host}")
-  private String seleniumHost;
+	@Value("${selenium.host}")
+	private String seleniumHost;
 
-  // seleniumに渡すオプションを定義
-  private static final ChromeOptions chromeOptions = new ChromeOptions()
-      .addArguments("--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage")
-      .setHeadless(true);
+	// seleniumに渡すオプションを定義
+	private static final ChromeOptions chromeOptions = new ChromeOptions()
+			.addArguments("--no-sandbox",
+					"--disable-gpu",
+					"--disable-dev-shm-usage",
+					"--headless");
 
-  private static final Duration DURATION_TIMEOUT_SECONDS = Duration.ofSeconds(30);
+	private static final Duration DURATION_TIMEOUT_SECONDS = Duration.ofSeconds(30);
 
-  /**
-   * chromeを操作するWebDriverを取得する
-   *
-   * @return WebDriverインスタンス
-   */
-  public RemoteWebDriver getWebDriver() {
-    try {
-      RemoteWebDriver remoteWebDriver = new RemoteWebDriver(new URL(seleniumHost), chromeOptions);
-      remoteWebDriver.manage().timeouts().implicitlyWait(DURATION_TIMEOUT_SECONDS);
-      return remoteWebDriver;
-    } catch (MalformedURLException e) {
-      throw new RuntimeException(e);
-    }
-  }
+	/**
+	 * chromeを操作するWebDriverを取得する
+	 *
+	 * @return WebDriverインスタンス
+	 */
+	public RemoteWebDriver getWebDriver() {
+		try {
+			RemoteWebDriver remoteWebDriver = new RemoteWebDriver(new URL(seleniumHost), chromeOptions);
+			remoteWebDriver.manage().timeouts().implicitlyWait(DURATION_TIMEOUT_SECONDS);
+			return remoteWebDriver;
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
