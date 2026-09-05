93cf7f7e281a8b52c47200889ecb7fda1fef7ae0
k-matsumoto-214 <keisuke.matsumoto214+github@gmail.com>
2026-09-05 22:51:49 +0900

    update for page

--- src/main/java/com/kirin/reservation/service/ReservationService.java
+++ src/main/java/com/kirin/reservation/service/ReservationService.java
@@ -6,113 +6,113 @@ import com.kirin.reservation.config.WebDriverConfig;
 import com.kirin.reservation.model.ReservationDate;
 import com.kirin.reservation.model.ReservationTime;
 import com.kirin.reservation.repository.database.ReservationDateRepository;
-import java.time.Clock;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ReservationService {
 
-  private final ReservationDateRepository reservationDateRepository;
+	private final ReservationDateRepository reservationDateRepository;
 
-  private final WebDriverConfig webDriverConfig;
+	private final WebDriverConfig webDriverConfig;
 
-  private final TimeConfig timeConfig;
+	private final TimeConfig timeConfig;
 
-  private final KirinWebConfig webConfig;
+	private final KirinWebConfig webConfig;
 
 
-  /**
-   * DBから予約情報を取得する
-   *
-   * @param targetName      予約対象者名
-   * @param targetDate      予約対象日付
-   * @param reservationTime 予約対象時間帯
-   * @return 予約情報
-   */
-  @Retryable
-  public ReservationDate findReservationTarget(String targetName, LocalDate targetDate,
-      ReservationTime reservationTime) {
+	/**
+	 * DBから予約情報を取得する
+	 *
+	 * @param targetName      予約対象者名
+	 * @param targetDate      予約対象日付
+	 * @param reservationTime 予約対象時間帯
+	 * @return 予約情報
+	 */
+	@Retryable
+	public ReservationDate findReservationTarget(String targetName, LocalDate targetDate, ReservationTime reservationTime) {
 
-    // DBから予約情報を取得
-    return reservationDateRepository.findByReservationDate(targetName, targetDate, reservationTime);
-  }
+		// DBから予約情報を取得
+		return reservationDateRepository.findByReservationDate(targetName, targetDate, reservationTime);
+	}
 
-  /**
-   * web予約をおこなってその結果(受付番号)を返す
-   *
-   * @param targetName      予約対象者名
-   * @param reservationTime 予約時間帯
-   * @param clock
-   * @return 予約成功のとき受付番号
-   */
-  public int reserve(String targetName, ReservationTime reservationTime, Clock clock) {
-    WebDriver webDriver = webDriverConfig.getWebDriver();
+	/**
+	 * web予約をおこなってその結果(受付番号)を返す
+	 *
+	 * @param targetName      予約対象者名
+	 * @param reservationTime 予約時間帯
+	 * @param clock
+	 * @return 予約成功のとき受付番号
+	 */
+	public int reserve(String targetName, ReservationTime reservationTime, Clock clock) {
+		WebDriver webDriver = webDriverConfig.getWebDriver();
 
-    try {
-      log.info("{}の予約を開始", targetName);
+		try {
+			log.info("{}の予約を開始", targetName);
 
-      webDriver.get(webConfig.url());
+			webDriver.get(webConfig.url());
 
-      // email入力
-      webDriver.findElement(webConfig.emailSelector()).sendKeys(webConfig.user());
+			// email入力
+			webDriver.findElement(webConfig.emailSelector()).sendKeys(webConfig.user());
 
-      // password入力
-      webDriver.findElement(webConfig.passwordSelector()).sendKeys(webConfig.password());
+			// password入力
+			webDriver.findElement(webConfig.passwordSelector()).sendKeys(webConfig.password());
 
-      // ログイン実行
-      webDriver.findElement(webConfig.loginSelector()).click();
+			// ログイン実行
+			webDriver.findElement(webConfig.loginSelector()).click();
 
-      log.info("ログイン");
+			log.info("ログイン");
 
-      // 予約開始時刻を取得
-      LocalDateTime targetTime = timeConfig.getTargetTime(reservationTime, clock);
+			// 予約開始時刻を取得
+			LocalDateTime targetTime = timeConfig.getTargetTime(reservationTime, clock);
 
-      // 予約開始時間まで待機
-      timeConfig.until(targetTime, clock);
+			// 予約開始時間まで待機
+			timeConfig.until(targetTime, clock);
 
-      log.info("予約開始");
+			log.info("予約開始");
 
-      // 予約開始時間になったら予約画面を開く
-      webDriver.get(webConfig.reservationUrl(reservationTime, clock));
+			// 予約開始時間になったら予約画面を開く
+			webDriver.get(webConfig.reservationUrl(reservationTime, clock));
 
-      // 予約対象者のチェックを確認する
-      boolean isChecked = webDriver.findElement(webConfig.userIdSelector()).isSelected();
+			// 予約対象者のチェックを確認する
+			boolean isChecked = webDriver.findElement(webConfig.userIdSelector()).isSelected();
 
-      if (isChecked) {
-        log.info("チェック済み");
-      }
+			if (isChecked) {
+				log.info("チェック済み");
+			}
 
-      if (!isChecked) {
-        log.info("チェックされていないので対象者をチェック");
-        webDriver.findElement(webConfig.userIdSelector()).click();
-      }
+			if (!isChecked) {
+				log.info("チェックされていないので対象者をチェック");
+				webDriver.findElement(webConfig.userIdSelector()).click();
+			}
 
-      // 予約実行
-      webDriver.findElement(webConfig.executeSelector()).click();
+			// 予約実行
+			webDriver.findElement(webConfig.executeSelector()).click();
 
-      // アラートの確認を受け入れる
-      webDriver.switchTo().alert().accept();
+			// アラートの確認を受け入れる
+			// コメントアウト
+//      webDriver.switchTo().alert().accept();
 
-      // 予約受付番号を取得する
-      final String reservationOrderString =
-          webDriver.findElement(webConfig.reservationOrderSelector()).getText();
+			// 予約受付番号を取得する
+			final String reservationOrderString = webDriver.findElement(webConfig.reservationOrderSelector()).getText();
 
-      return Integer.parseInt(reservationOrderString);
+			return Integer.parseInt(reservationOrderString);
 
-    } catch (Exception e) {
-      log.error("{}の予約中にエラー発生 原因: {}", targetName, e.toString());
-      throw new RuntimeException(e);
+		} catch (Exception e) {
+			log.error("{}の予約中にエラー発生 原因: {}", targetName, e.toString());
+			throw new RuntimeException(e);
 
-    } finally {
-      webDriver.quit();
-    }
-  }
+		} finally {
+			webDriver.quit();
+		}
+	}
 }
